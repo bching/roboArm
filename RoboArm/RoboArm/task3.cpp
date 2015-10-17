1a3220c05f68a4a1f322beca6f9bb85833a6a1dc
Boyd <chingboyd@gmail.com>
2015-10-17 03:37:22 -0800

    Added - ball, disk(place on), inputkeys, mousemove

--- RoboArm/RoboArm/task3.cpp
+++ RoboArm/RoboArm/task3.cpp
@@ -1,18 +1,26 @@
 //
 //Comment out for pc
-//#include <GLUT/glut.h>
+#include <GLUT/glut.h>
+#include <SDL2/SDL.h>
+// Extension libraries for SDL2 to implement sound
+#include <SDL2_mixer/SDL_mixer.h>
+#include <SDL2_image/SDL_image.h>
+
 //=======================================================//
 //comment out for mac
-#include <GL/glut.h>
-#include <GL/glu.h>
-#include <GL/gl.h>
+//#include <GL/glut.h>
+//#include <GL/glu.h>
+//#include <GL/gl.h>
 //======================================================//
 //======================================================//
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
+#include <string>
 
-float xpos, ypos, xrot, yrot, angle = 0.0;
+float xpos, ypos, zpos, xrot, yrot, angle = 0.0;
+float baserot, armrot = 0.0;
+//Radius from camera to point of focus 
 float cradius = 10.0f;
 float lastx, lasty;
 
@@ -34,16 +42,49 @@ void enable(void){
 	glShadeModel(GL_SMOOTH);
 }
 
+void drawBall(void){
+	glPushAttrib(GL_CURRENT_BIT);
+	glColor3d(1, 0, 0);
+
+	glPushMatrix();
+		glTranslated(2.0, 0.0, 0.5);
+		glutSolidSphere(0.5, 50, 50);
+	glPopMatrix();
+
+	glPopAttrib();
+}
+
+void drawPad(void){
+	glPushAttrib(GL_CURRENT_BIT);
+
+	glColor3d(1, 1, 1);
+	glPushMatrix();
+		glTranslated(1.5, -0.5, 2.0);
+		glScaled(1.0, 0.1, 1.0);
+		glutWireSphere(0.5, 50, 50);
+	glPopMatrix();
+
+	glPopAttrib();
+}
+
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	enable();
 	glLoadIdentity();
 
-	/** Make calls to draw objects in here **/
+	glTranslatef(0.0f, 0.0f, -cradius);
+	glRotatef(xrot, 1.0, 0.0, 0.0);
+
+	drawBall();
+	drawPad();
+
+//	glRotatef(yrot, 0.0, 1.0, 0.0);
+//	glTranslated(-xpos, 0.0, -zpos);
 
 
 	glutSwapBuffers();
 }
+
 
 void reshape(int w, int h) {
 	const float ar = (float) w / (float) h;
@@ -76,6 +117,25 @@ float drawCollision(float color)
 }
 
 void keyboard(unsigned char key, int x, int y) {
+
+	switch(key){
+		case 'a':
+			baserot++;
+			break;
+		case 'd':
+			baserot--;
+			break;
+		case 'w':
+			armrot++;
+			break;
+		case 's':
+			armrot++;
+			break;
+	}
+
+	if(key == 27)
+		exit(0);
+
 }
 
 void mouseMovement(int x, int y) {
@@ -101,9 +161,9 @@ int main(int argc, char **argv) {
 
 	init();
 
+	glutReshapeFunc(reshape);
 	glutDisplayFunc(display);
 	glutIdleFunc(display);
-	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyboard);
 //	glutSpecialFunc(special);
 
